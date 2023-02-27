36de3db87f56124b42984fa4e172a4c28577989d
mengesha1 <mengtseg@gmail.com>
2023-02-27 10:00:41 -0500

    Update main.tf
    configmap enable true

--- main.tf
+++ main.tf
@@ -62,4 +62,6 @@ data "aws_eks_cluster" "cluster" {
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
+  create_aws_auth_configmap = true 
+  manage_aws_auth_configmap = true  
 }
