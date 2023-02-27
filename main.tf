6aa2e7602759930ae87f25441bfc34193ed60d45
mengesha1 <mengtseg@gmail.com>
2023-02-27 11:16:48 -0500

    Update main.tf
    provider kubernetes resource added

--- main.tf
+++ main.tf
@@ -62,6 +62,10 @@ data "aws_eks_cluster" "cluster" {
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
-  create_aws_auth_configmap = true 
-  manage_aws_auth_configmap = true  
 }
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.default.token
+}
+  
