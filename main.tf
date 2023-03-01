f1b304e8f21830ed6b6b0422b447b2bbd06b62e9
mengesha1 <mengtseg@gmail.com>
2023-02-28 20:02:06 -0500

    Update main.tf
    comment on aws cluster

--- main.tf
+++ main.tf
@@ -56,17 +56,11 @@ module "eks" {
   ]
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
+#data "aws_eks_cluster" "cluster" {
+#  name = module.eks.cluster_id
+#}
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
+
   
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.default.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.default.token
-}
+
   
